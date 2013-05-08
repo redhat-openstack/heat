fe5ae0a03acc58440937595dc82dab26f8dfc09c
Jeff Peeler <jpeeler@redhat.com>
2013-05-08 12:27:35 -0400

    Switch to using M2Crypto
    This patch uses M2Crypto instead of PyCrypto to perform encryption
    and decryption of user authentication information.

--- heat/common/crypt.py
+++ heat/common/crypt.py
@@ -13,10 +13,11 @@
 #    under the License.
 
 import base64
-from Crypto.Cipher import AES
+import M2Crypto
+from os import urandom
+
 from oslo.config import cfg
 
-from heat.openstack.common.crypto import utils
 from heat.openstack.common import log as logging
 
 
@@ -34,19 +35,12 @@ logger = logging.getLogger(__name__)
 def encrypt(auth_info):
     if auth_info is None:
         return None, None
-    sym = utils.SymmetricCrypto()
-    res = sym.encrypt(cfg.CONF.auth_encryption_key[:32],
-                      auth_info, b64encode=True)
-    return 'oslo_decrypt_v1', res
-
-
-def oslo_decrypt_v1(auth_info):
-    if auth_info is None:
-        return None
-    sym = utils.SymmetricCrypto()
-    return sym.decrypt(cfg.CONF.auth_encryption_key[:32],
-                       auth_info, b64decode=True)
-
+    iv = urandom(16)
+    cipher = M2Crypto.EVP.Cipher(alg='aes_128_cbc',
+                                 key=cfg.CONF.auth_encryption_key[:32], iv=iv,
+                                 op=1) # 1 is encode
+    res = base64.b64encode(iv + cipher.update(auth_info) + cipher.final())
+    return 'heat_decrypt', res
 
 def heat_decrypt(auth_info):
     """Decrypt function for data that has been encrypted using an older
@@ -60,7 +54,9 @@ def heat_decrypt(auth_info):
     if auth_info is None:
         return None
     auth = base64.b64decode(auth_info)
-    iv = auth[:AES.block_size]
-    cipher = AES.new(cfg.CONF.auth_encryption_key[:32], AES.MODE_CFB, iv)
-    res = cipher.decrypt(auth[AES.block_size:])
+    iv = auth[:16]
+    cipher = M2Crypto.EVP.Cipher(alg='aes_128_cbc',
+                                 key=cfg.CONF.auth_encryption_key[:32], iv=iv,
+                                 op=0) # 0 is decode
+    res = cipher.update(auth[16:]) + cipher.final()
     return res
