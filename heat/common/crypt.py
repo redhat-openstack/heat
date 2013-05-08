389c459f1b0729adc395e7034edfb6a568eb8e94
Jeff Peeler <jpeeler@redhat.com>
2013-05-08 12:27:35 -0400

    Switch to using M2Crypto
    This patch uses M2Crypto instead of PyCrypto to perform encryption
    and decryption of user authentication information.

--- heat/common/crypt.py
+++ heat/common/crypt.py
@@ -14,7 +14,7 @@
 #    under the License.
 
 import base64
-from Crypto.Cipher import AES
+import M2Crypto
 from os import urandom
 
 from oslo.config import cfg
@@ -36,9 +36,11 @@ logger = logging.getLogger(__name__)
 def encrypt(auth_info):
     if auth_info is None:
         return None
-    iv = urandom(AES.block_size)
-    cipher = AES.new(cfg.CONF.auth_encryption_key[:32], AES.MODE_CFB, iv)
-    res = base64.b64encode(iv + cipher.encrypt(auth_info))
+    iv = urandom(16)
+    cipher = M2Crypto.EVP.Cipher(alg='aes_128_cbc',
+                                  key=cfg.CONF.auth_encryption_key[:32], iv=iv,
+                                  op=1) # 1 is encode
+    res = base64.b64encode(iv + cipher.update(auth_info) + cipher.final())
     return res
 
 
@@ -46,7 +48,9 @@ def decrypt(auth_info):
     if auth_info is None:
         return None
     auth = base64.b64decode(auth_info)
-    iv = auth[:AES.block_size]
-    cipher = AES.new(cfg.CONF.auth_encryption_key[:32], AES.MODE_CFB, iv)
-    res = cipher.decrypt(auth[AES.block_size:])
+    iv = auth[:16]
+    cipher = M2Crypto.EVP.Cipher(alg='aes_128_cbc',
+                                 key=cfg.CONF.auth_encryption_key[:32], iv=iv,
+                                 op=0) # 0 is decode
+    res = cipher.update(auth[16:]) + cipher.final()
     return res
